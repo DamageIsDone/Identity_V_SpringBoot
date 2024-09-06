9388bd68ed7a523b35ff30bc08edde4901fd7eba
Xsy <121719968+DamageIsDone@users.noreply.github.com>
2024-09-06 11:14:48 +0800

    Create I_HRepository.java

---
+++ src/main/java/com/example/mysite/repository/I_HRepository.java
@@ -0,0 +1,7 @@
+package com.example.mysite.repository;
+
+import com.example.mysite.classes.I_H;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface I_HRepository extends JpaRepository<I_H, I_H.I_HKey> {
+}
