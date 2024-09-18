e288bc32198f13f2920cadabee1bb0cffe1f1b84
XSY <624848971@qq.com>
2024-09-18 15:43:33 +0800

    完成了战绩删除功能

--- src/main/java/com/example/mysite/controller/GameController.java
+++ src/main/java/com/example/mysite/controller/GameController.java
@@ -3,11 +3,9 @@ package com.example.mysite.controller;
 import com.example.mysite.classes.Game;
 import com.example.mysite.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +36,19 @@ public class GameController {
                 "CASE WHEN result4 = TRUE THEN 1 ELSE 0 END) AS true_count " +
                 "FROM Game WHERE game_id = ?";
         return jdbcTemplate.queryForObject(sql, new Object[]{game_id}, Integer.class);
+    }
+
+    @DeleteMapping("/delete")
+    public Game deleteGame(@RequestParam("game_id") Integer game_id) {
+        String selectSql = "SELECT * FROM Game WHERE game_id = ?";
+        Game game = jdbcTemplate.queryForObject(selectSql, new BeanPropertyRowMapper<>(Game.class), game_id);
+
+        if (game != null) {
+            String sql = "DELETE FROM Game WHERE game_id = ?";
+            jdbcTemplate.update(sql, game_id);
+        }
+
+        return game;
     }
 
 }
